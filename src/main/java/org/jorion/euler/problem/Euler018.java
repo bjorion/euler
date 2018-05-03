e85e922fc1f8d47afd1c538ff2a9641c3b76d34a
bjorion <bjorion@gmail.com>
2018-05-03 21:08:49 +0200

    018

--- src/main/java/org/jorion/euler/problem/Euler018.java
+++ src/main/java/org/jorion/euler/problem/Euler018.java
@@ -3,22 +3,29 @@ package org.jorion.euler.problem;
 import org.jorion.euler.util.Utils;
 
 /**
- * By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+ * By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from
+ * top to bottom is 23.
+ * 
  * <pre>
  *    3
  *   7 4
  *  2 4 6
  * 8 5 9 3
  * </pre>
+ * 
  * That is, 3 + 7 + 4 + 9 = 23.
  *
- * Find the maximum total from top to bottom of the triangle below:<p>
- * NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+ * Find the maximum total from top to bottom of the triangle below:
+ * <p>
+ * NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem
+ * 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and
+ * requires a clever method! ;o)
  */
-public class Euler018 {
+public class Euler018
+{
 
-	// --- Constants ---
-	// @formatter:off
+    // --- Constants ---
+    // @formatter:off
 	@SuppressWarnings("unused")
 	private static final int[] T0 = {
 			   3,
@@ -45,108 +52,121 @@ public class Euler018 {
 	};
 	// @formatter:on
 
-	// --- Methods ---
-	public static void main(String[] args) {
+    // --- Methods ---
+    public static void main(String[] args)
+    {
+        long res; // 1074
+        long delta;
 
-		long res; // 1074
-		long delta;
+        Utils.start();
+        res = calc1(T1);
+        delta = Utils.stop();
+        Utils.print("Approx. (wrong)", res, delta);
 
-		Utils.start();
-		res = calc1(T1);
-		delta = Utils.stop();
-		Utils.print("Wrong result", res, delta);
+        Utils.start();
+        res = calc2(T1);
+        delta = Utils.stop();
+        Utils.print("Brute Force    ", res, delta);
 
-		Utils.start();
-		res = calc2(T1);
-		delta = Utils.stop();
-		Utils.print("Brute Force ", res, delta);
+        Utils.start();
+        res = calc3(T1);
+        delta = Utils.stop();
+        Utils.print("Smartest (best)", res, delta);
+    }
 
-		Utils.start();
-		res = calc3(T0);
-		delta = Utils.stop();
-		Utils.print("", res, delta);
-	}
+    /** By closest neighbour (wrong solution). */
+    private static int calc1(int[] data)
+    {
+        int nLine = computeLines(data.length);
+        int sum = getElem(data, 0, 0);
+        int j = 0;
+        for (int i = 1; i < nLine; i++) {
+            int val1 = getElem(data, i, j);
+            int val2 = getElem(data, i, j + 1);
+            if (val2 > val1) {
+                j++;
+                sum += val2;
+            }
+            else {
+                sum += val1;
+            }
+        }
+        return sum;
+    }
 
-	/** By closest neighbour (wrong solution). */
-	private static int calc1(int[] data) {
+    /**
+     * Check all possible combinations. Best path: 10111110010011 (= 12179) (75 =>
+     * 64-82-87-82-75-73-28-83-32-91-78-58-73-93)
+     * <p>
+     * Complexity: O(nLine²)
+     */
+    private static int calc2(int[] data)
+    {
+        int best = 0;
+        // int bestPath = 0;
+        int nLine = computeLines(data.length);
+        int nPath = (int) Math.pow(2, nLine - 1);
 
-		int nLine = computeLines(data.length);
-		int sum = getElem(data, 0, 0);
-		int j = 0;
-		for (int i = 1; i < nLine; i++) {
-			int val1 = getElem(data, i, j);
-			int val2 = getElem(data, i, j + 1);
-			if (val2 > val1) {
-				j++;
-				sum += val2;
-			} else {
-				sum += val1;
-			}
-		}
-		return sum;
-	}
+        for (int path = 0; path < nPath; path++) {
+            int sum = getElem(data, 0, 0);
+            int j = 0;
+            int mask = 1;
+            for (int i = 1; i < nLine; i++) {
+                if ((path & mask) > 0) {
+                    j++;
+                }
+                mask <<= 1;
+                int val = getElem(data, i, j);
+                sum += val;
+            }
+            if (sum > best) {
+                best = sum;
+                // bestPath = path;
+            }
+        }
+        return best;
+    }
 
-	/**
-	 * Check all possible combinations.
-	 * Best path: 10111110010011 (= 12179)
-	 * (75 => 64-82-87-82-75-73-28-83-32-91-78-58-73-93)
-	 */
-	private static int calc2(int[] data) {
+    /**
+     * Best solution: keep only the best solution for every element.
+     * <p>
+     * Complexity: O(nLine)
+     */
+    private static int calc3(int[] data)
+    {
+        int nLine = computeLines(data.length);
+        int[] val1 = new int[] { getElem(data, 0, 0) };
+        for (int i = 1; i < nLine; i++) {
+            int[] val2 = new int[i + 1];
+            for (int j = 0; j < i + 1; j++) {
+                int left = (j - 1 >= 0) ? val1[j - 1] : 0;
+                int right = (j < i) ? val1[j] : 0;
+                val2[j] = getElem(data, i, j) + Math.max(left, right);
+            }
+            val1 = val2;
+        }
 
-		int best = 0;
-		// int bestPath = 0;
-		int nLine = computeLines(data.length);
-		int nPath = (int) Math.pow(2, nLine - 1);
+        int best = 0;
+        for (int val : val1) {
+            if (val > best) {
+                best = val;
+            }
+        }
+        return best;
+    }
 
-		for (int path = 0; path < nPath; path++) {
-			int sum = getElem(data, 0, 0);
-			int j = 0;
-			int mask = 1;
-			for (int i = 1; i < nLine; i++) {
-				if ((path & mask) > 0) {
-					j++;
-				}
-				mask <<= 1;
-				int val = getElem(data, i, j);
-				sum += val;
-			}
-			if (sum > best) {
-				best = sum;
-				// bestPath = path;
-			}
-		}
-		return best;
-	}
+    /**
+     * i = [0, line[; j = [0, i]
+     *
+     * @return element corresponding to indexes i, j
+     */
+    private static int getElem(int[] data, int i, int j)
+    {
+        return data[i * (i + 1) / 2 + j];
+    }
 
-	private static int calc3(int[] data) {
-
-		int nLine = computeLines(data.length);
-		int sum = getElem(data, 0, 0);
-		for (int i = 1; i < nLine; i++) {
-			int best = 0;
-			for (int j = 0; j <= i; j++) {
-				int val = getElem(data, i, j);
-				if (val > best) {
-					best = val;
-				}
-			}
-			sum += best;
-		}
-		return sum;
-	}
-
-	/**
-	 * i = [0, line[; j = [0, i]
-	 *
-	 * @return element corresponding to indexes i, j
-	 */
-	private static int getElem(int[] data, int i, int j) {
-
-		return data[i * (i + 1) / 2 + j];
-	}
-
-	private static int computeLines(int len) {
-
-		return (int) (-1 + Math.sqrt(1 + 8 * len)) / 2;
-	}
+    private static int computeLines(int len)
+    {
+        return (int) (Math.sqrt(8 * len + 1) - 1) / 2;
+    }
 }
