562fa2824918d92e58f4ce3e356bfce3d9cc05f7
bjorion <bjorion@gmail.com>
2018-05-12 21:42:33 +0200

    030

--- src/main/java/org/jorion/euler/problem/Euler007.java
+++ src/main/java/org/jorion/euler/problem/Euler007.java
@@ -7,34 +7,33 @@ import org.jorion.euler.util.Utils;
  * <p>
  * What is the 10 001st prime number?
  */
-public class Euler007 {
+public class Euler007
+{
+    public static void main(String[] args)
+    {
+        final int max = 10_001;
 
-	public static void main(String[] args) {
+        long res; // 104743
+        long delta;
 
-		final int max = 10_001;
+        Utils.start();
+        res = calc1(max);
+        delta = Utils.stop();
+        Utils.print("Simple", res, delta);
+    }
 
-		long res; // 104743
-		long delta;
-
-		Utils.start();
-		res = calc1(max);
-		delta = Utils.stop();
-		Utils.print("Simple", res, delta);
-	}
-
-	private static long calc1(int max) {
-
-		long res = 0;
-		long i = 3;
-		int count = 1;
-		while (count < max) {
-			if (Utils.isPrime(i)) {
-				res = i;
-				count++;
-			}
-			i += 2;
-		}
-		return res;
-	}
-
+    private static long calc1(int max)
+    {
+        long res = 0;
+        long i = 3;
+        int count = 1;
+        while (count < max) {
+            if (Utils.isPrime(i)) {
+                res = i;
+                count++;
+            }
+            i += 2;
+        }
+        return res;
+    }
 }
---
+++ src/main/java/org/jorion/euler/problem/Euler030.java
@@ -0,0 +1,97 @@
+package org.jorion.euler.problem;
+
+import org.jorion.euler.util.Utils;
+
+/**
+ * Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+ * 
+ * <pre>
+ *    1634 = 1^4 + 6^4 + 3^4 + 4^4
+ *    8208 = 8^4 + 2^4 + 0^4 + 8^4
+ *    9474 = 9^4 + 4^4 + 7^4 + 4^4
+ * </pre>
+ * 
+ * As 1 = 1^4 is not a sum it is not included.
+ * <p>
+ * The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+ * <p>
+ * Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+ */
+public class Euler030
+{
+    // --- Constants ---
+    private static final int ASCII_0 = 48;
+
+    // --- Methods ---
+    public static void main(String[] args)
+    {
+        final int power = 5;
+
+        long res;
+        long delta;
+
+        Utils.start();
+        res = calc1(power);
+        delta = Utils.stop();
+        Utils.print("Simple  ", res, delta);
+        
+        Utils.start();
+        res = calc2(power);
+        delta = Utils.stop();
+        Utils.print("Simple 2", res, delta);
+    }
+
+    /**
+     * Use Integer.toString()
+     */
+    private static long calc1(int power)
+    {
+        int max = 1_000_000;
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)Math.pow(i, power);
+        }
+        
+        long sum = 0;
+        for (int i = 10; i < max; i++) {
+            long val = 0;
+            String str = Integer.toString(i);
+            for (int j = 0; j < str.length(); j++) {
+                int digit = str.charAt(j) - ASCII_0;
+                val += arr[digit];
+            }
+            if (i == val) {
+                sum+= val;
+                // System.out.println(val);
+            }
+        }
+        return sum;
+    }
+    
+    /**
+     * Use moduleo
+     */
+    private static long calc2(int power)
+    {
+        int max = 1_000_000;
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)Math.pow(i, power);
+        }
+        
+        long sum = 0;
+        for (int i = 10; i < max; i++) {
+            long val = 0;
+            int n = i;
+            while (n > 0) {
+                int digit = n % 10;
+                val += arr[digit];
+                n = n / 10;
+            }
+            if (i == val) {
+                sum+= val;
+            }
+        }
+        return sum;
+    }
+}
