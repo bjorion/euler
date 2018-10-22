3bb96f84fde5a1167212df26585d7a57b741920d
Bernard Jorion <bernard.jorion@sigedis.fgov.be>
2018-10-22 16:35:57 +0200

    062

--- src/main/java/org/jorion/euler/problem/Euler062.java
+++ src/main/java/org/jorion/euler/problem/Euler062.java
@@ -1,5 +1,6 @@
 package org.jorion.euler.problem;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,19 +22,24 @@ public class Euler062 {
 	public static void main(String[] args) {
 
 		final int max = 5;
-		long res; // 127035954683, 1272ms
+		String res; // 127035954683
 		long delta;
 
 		Utils.start();
 		res = calc1(max);
 		delta = Utils.stop();
-		Utils.print("Simple ", res, delta);
+		Utils.print("Simple ", res, delta); // 1200ms
+
+		Utils.start();
+		res = calc2(max);
+		delta = Utils.stop();
+		Utils.print("Best   ", res, delta); // 7ms
 	}
 
 	/**
 	 * Here we precompute the cubes and for each cube, we check how many other cubes (strictly greater) are permutations of the current one.
 	 */
-	private static long calc1(int max) {
+	private static String calc1(int max) {
 
 		int start = 345;
 		int end = 9999;
@@ -70,7 +76,47 @@ public class Euler062 {
 				}
 			}
 		}
-		return Long.parseLong(res);
+		return res;
 	}
 
+	/**
+	 * Improvement: we avoid checking the permutation my ordering the string at the creation.
+	 */
+	private static String calc2(int max) {
+
+		int start = 345;
+		int end = 9999;
+
+		String res = "0";
+		Map<String, Dual> map = new HashMap<>();
+		for (int i = start; i <= end; i++) {
+			long val = ((long) i) * i * i;
+			String cube = Long.toString(val);
+			char[] arr = cube.toCharArray();
+			Arrays.sort(arr);
+			String key = new String(arr);
+			Dual dual = map.getOrDefault(key, new Dual(0, cube));
+			dual = new Dual(dual.count + 1, dual.base);
+			map.put(key, dual);
+			if (dual.count == max) {
+				res = dual.base;
+				break;
+			}
+		}
+
+		return res;
+	}
+
+	static class Dual {
+
+		final int count;
+
+		final String base;
+
+		public Dual(int count, String base) {
+
+			this.count = count;
+			this.base = base;
+		}
+	}
 }
