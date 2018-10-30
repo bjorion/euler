0cd28e5ae48c029d95d5ae3fa118e46bba19dbcc
Bernard Jorion <bernard.jorion@sigedis.fgov.be>
2018-10-30 11:32:45 +0100

    102

--- src/main/java/org/jorion/euler/problem/Euler102.java
+++ src/main/java/org/jorion/euler/problem/Euler102.java
@@ -30,10 +30,32 @@ public class Euler102 {
 	public static void main(String[] args)
 			throws Exception {
 
-		long res;
+		long res; // 228
 		long delta;
 
 		List<String> lines = Utils.readLines("p102_triangles.txt");
+
+		Utils.start();
+		res = calc1(lines);
+		delta = Utils.stop();
+		Utils.print("Algorithm ", res, delta);
+	}
+
+	/**
+	 * Let A, B and C be the three summits of the triangle. We compute AB, BC and AC the three lines (that include the triangle sides). For each line,
+	 * we check if the origin and the 3r summit are on the same side. If it's always yes, then the origin is inside the triangle.
+	 * <p>
+	 * Hypothesis:
+	 * <ul>
+	 * <li>if the origin belongs to the side of a triangle, then it is not inside
+	 * <li>the triangles are "well-behaved", that the 3 points are never aligned
+	 * </ul>
+	 *
+	 * @param lines A list of 6 integers coordinates of the summits
+	 * @return the number of triangles containing the origin
+	 */
+	private static int calc1(List<String> lines) {
+
 		List<Triangle> triangles = new ArrayList<>();
 		for (String line : lines) {
 			String[] vals = line.split(",");
@@ -43,53 +65,126 @@ public class Euler102 {
 			triangles.add(new Triangle(a, b, c));
 		}
 
-		Utils.start();
-		res = calc1(triangles);
-		delta = Utils.stop();
-		Utils.print("Algorithm ", res, delta);
+		int res = 0;
+		for (Triangle t : triangles) {
+			if (t.containsOrigin()) {
+				res++;
+			}
+		}
+		return res;
 	}
 
 	/**
-	 * Let A, B and C be the three summits of the triangle. We compute AB, BC and AC the three lines (that include the triangle sides).
+	 * Triangle class, made of 3 Points.
 	 */
-	private static long calc1(List<Triangle> triangles) {
-
-		System.out.println(triangles.size());
-		return 0;
-	}
-
 	static class Triangle {
 
-		final Point a;
+		final Point a, b, c;
 
-		final Point b;
+		final Line ab, bc, ac;
 
-		final Point c;
-
-		public Triangle(Point a, Point b, Point c) {
+		Triangle(Point a, Point b, Point c) {
 
 			this.a = a;
 			this.b = b;
 			this.c = c;
+
+			this.ab = new Line(a, b);
+			this.bc = new Line(b, c);
+			this.ac = new Line(a, c);
+		}
+
+		boolean containsOrigin() {
+
+			boolean ok = (Math.signum(ab.computeSidePoint(c)) == Math.signum(ab.computeSideOrigin()));
+			if (ok) {
+				ok = (Math.signum(bc.computeSidePoint(a)) == Math.signum(bc.computeSideOrigin()));
+			}
+			if (ok) {
+				ok = (Math.signum(ac.computeSidePoint(b)) == Math.signum(ac.computeSideOrigin()));
+			}
+			return ok;
+		}
+
+		@Override
+		public String toString() {
+
+			return "Triangle[A=" + a + ", B=" + b + ", C=" + c + "]";
 		}
 	}
 
+	/**
+	 * Line class, made of 2 Points.
+	 */
+	static class Line {
+
+		final double a;
+
+		final double b;
+
+		final double c;
+
+		Line(Point p1, Point p2) {
+
+			if (p1.x != p2.x) {
+				a = -((double) (p1.y - p2.y)) / (p1.x - p2.x);
+				b = 1;
+				c = -((double) (p1.x * p2.y - p1.y * p2.x)) / (p1.x - p2.x);
+			}
+			// vertical line
+			else {
+				a = 1;
+				b = 0;
+				c = -p1.x;
+			}
+		}
+
+		double computeSidePoint(Point p) {
+
+			double res = a * p.x + b * p.y + c;
+			if (res == 0) {
+				System.out.println(String.format("WARNING: line %s contains the point %s", this.toString(), p.toString()));
+			}
+			return res;
+		}
+
+		double computeSideOrigin() {
+
+			return c;
+		}
+
+		@Override
+		public String toString() {
+
+			return "Line[" + a + "x + " + b + "y + " + c + "]";
+		}
+	}
+
+	/**
+	 * Point class.
+	 */
 	static class Point {
 
 		final int x;
 
 		final int y;
 
-		public Point(int x, int y) {
+		Point(int x, int y) {
 
 			this.x = x;
 			this.y = y;
 		}
 
-		public Point(String x, String y) {
+		Point(String x, String y) {
 
 			this.x = Integer.parseInt(x);
 			this.y = Integer.parseInt(y);
+		}
+
+		@Override
+		public String toString() {
+
+			return "Point[x=" + x + ", y=" + y + "]";
 		}
 	}
 }
